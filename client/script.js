846273447c21b2840a83c4c8fdca3b8604665c25
PW <pwelliver@yahoo.com>
2020-06-21 15:46:13 +0200

    add components modal, fix reselect modal function

--- client/script.js
+++ client/script.js
@@ -18,6 +18,11 @@ const appsClose = document.getElementById('apps-close');
 const appsOpen = document.getElementById('apps-open');
 const appsModal = document.getElementById('apps-modal');
 
+// components modal
+const componentsClose = document.getElementById('components-close');
+const componentsOpen = document.getElementById('components-open');
+const componentsModal = document.getElementById('components-modal');
+
 // Toggle nav
 toggle.addEventListener('click', () =>
   document.body.classList.toggle('show-nav')
@@ -26,17 +31,24 @@ toggle.addEventListener('click', () =>
 // Show modal
 //open.addEventListener('click', () => modal.classList.add('show-modal'));
 // Show games-modal
-gamesOpen.addEventListener('click', () =>
-  gamesModal.classList.add('show-games-modal')
-);
+gamesOpen.addEventListener('click', () => {
+  clearAllModals();
+  gamesModal.classList.add('show-games-modal');
+});
 // Show voice-video-modal
-voiceVideoOpen.addEventListener('click', () =>
-  voiceVideoModal.classList.add('show-voice-video-modal')
-);
+voiceVideoOpen.addEventListener('click', () => {
+  clearAllModals();
+  voiceVideoModal.classList.add('show-voice-video-modal');
+});
 // Show apps-modal
 appsOpen.addEventListener('click', () => {
-  console.log('appsOpen');
+  clearAllModals();
   appsModal.classList.add('show-apps-modal');
+});
+// Show components-modal
+componentsOpen.addEventListener('click', () => {
+  clearAllModals();
+  componentsModal.classList.add('show-components-modal');
 });
 
 // Hide modal
@@ -52,6 +64,10 @@ voiceVideoClose.addEventListener('click', () =>
 // Hide apps-modal
 appsClose.addEventListener('click', () =>
   appsModal.classList.remove('show-apps-modal')
+);
+// Hide components-modal
+componentsClose.addEventListener('click', () =>
+  componentsModal.classList.remove('show-components-modal')
 );
 
 // Hide modal on outside click
@@ -72,11 +88,34 @@ window.addEventListener('click', (e) => {
     voiceVideoModal.classList.remove('show-voice-video-modal');
   }
 
-  // voice-video-modal
+  // apps-modal
   if (e.target === appsModal) {
     appsModal.classList.remove('show-apps-modal');
+  }
+
+  // components-modal
+  if (e.target === componentsModal) {
+    componentsModal.classList.remove('show-components-modal');
   }
   /*
   e.target == modal ? modal.classList.remove('show-modal') : false;
   */
 });
+
+function clearAllModals() {
+  if (gamesModal.classList.contains('show-games-modal')) {
+    gamesModal.classList.remove('show-games-modal');
+  }
+
+  if (voiceVideoModal.classList.contains('show-voice-video-modal')) {
+    voiceVideoModal.classList.remove('show-voice-video-modal');
+  }
+
+  if (appsModal.classList.contains('show-apps-modal')) {
+    appsModal.classList.remove('show-apps-modal');
+  }
+
+  if (componentsModal.classList.contains('show-components-modal')) {
+    componentsModal.classList.remove('show-components-modal');
+  }
+}
